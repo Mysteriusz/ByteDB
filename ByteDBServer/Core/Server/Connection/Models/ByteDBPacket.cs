0e73a778ddaa2e57675541fd70bfd4022c7f61d2
Mysteriusz <wixxxiw730@gmail.com>
2025-01-02 17:28:30 +0100

    ByteDBPacket

--- ByteDBServer/Core/Server/Connection/Models/ByteDBPacket.cs
+++ ByteDBServer/Core/Server/Connection/Models/ByteDBPacket.cs
@@ -1,14 +1,37 @@
-﻿using System.Collections.Generic;
+﻿using ByteDBServer.Core.DataTypes;
+using System.Collections.Generic;
 using System.IO;
 
 namespace ByteDBServer.Core.Server.Connection.Models
 {
     internal class ByteDBPacket : List<byte>
     {
-        public int Size { get; set; }
+        //
+        // ----------------------------- PARAMETERS ----------------------------- 
+        //
 
-        public List<byte> Header { get; }
+        public Int2 Size 
+        {
+            get 
+            {
+                return new Int2(Payload.Count); 
+            }
+        }
+        public List<byte> Header
+        {
+            get
+            {
+                List<byte> _header = new List<byte>();
+                _header.AddRange((byte[])Size);
+
+                return _header;
+            }
+        }
         public List<byte> Payload { get; set; }
+        
+        //
+        // ----------------------------- CONSTRUCTORS ----------------------------- 
+        //
 
         public ByteDBPacket() { }
         public ByteDBPacket(IEnumerable<byte> payload)
@@ -16,13 +39,23 @@ namespace ByteDBServer.Core.Server.Connection.Models
             payload = Payload;
         }
 
+        //
+        // ----------------------------- METHODS ----------------------------- 
+        //
+
         public void Write(Stream stream)
         {
-            
+            List<byte> packet = GetPacket(this);
+            stream.Write(packet.ToArray(), 0, packet.Count);
         }
-        public void Read()
+        public static List<byte> GetPacket(ByteDBPacket packet)
         {
+            List<byte> _packet = new List<byte>();
 
+            _packet.AddRange(packet.Header);
+            _packet.AddRange(packet.Payload);
+            
+            return _packet;
         }
     }
 }
