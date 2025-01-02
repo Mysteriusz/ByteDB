84e07f1400029d91d0912dc5cbf933d45432b464
Mysteriusz <wixxxiw730@gmail.com>
2025-01-02 19:35:33 +0100

    ServerVersion property

---
+++ ByteDBServer/Core/Config/ByteDBServerConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml;
+
+namespace ByteDBServer.Core.Config
+{
+    internal static class ByteDBServerConfig
+    {
+        public const string ConfigPath = "Core\\Config\\ServerConfig.xml";
+
+        public static void InitializeConfig()
+        {
+            XmlDocument config = new XmlDocument();
+            config.LoadXml(ConfigPath);
+
+            ServerVersion = config.Attributes.GetNamedItem("ServerVersion").Value;
+        }
+
+        public static string ServerVersion { get; private set; }
+    }
+}
--- ByteDBServer/Core/Server/ByteDBServer.cs
+++ ByteDBServer/Core/Server/ByteDBServer.cs
@@ -1,15 +1,20 @@
-﻿using ByteDBServer.Core.Services;
+﻿using ByteDBServer.Core.Config;
+using ByteDBServer.Core.Services;
 using System.ServiceProcess;
 
 namespace ByteDBServer
 {
     internal static class ByteDBServer
     {
+        public static string Version => ByteDBServerConfig.ServerVersion;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         static void Main()
         {
+            ByteDBServerConfig.InitializeConfig();
+
             ServiceBase[] ServicesToRun;
             ServicesToRun = new ServiceBase[]
             {
