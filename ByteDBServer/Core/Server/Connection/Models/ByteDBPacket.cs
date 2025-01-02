a5d3e40ac45a5747ece60c2e5d320bb4bd89e96a
Mysteriusz <wixxxiw730@gmail.com>
2025-01-02 18:15:25 +0100

    models

--- ByteDBServer/Core/Server/Connection/Models/ByteDBPacket.cs
+++ ByteDBServer/Core/Server/Connection/Models/ByteDBPacket.cs
@@ -1,6 +1,7 @@
 ﻿using ByteDBServer.Core.DataTypes;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 
 namespace ByteDBServer.Core.Server.Connection.Models
 {
@@ -56,6 +57,37 @@ namespace ByteDBServer.Core.Server.Connection.Models
             _packet.AddRange(packet.Payload);
             
             return _packet;
+        }
+
+        //
+        // ----------------------------- OVERRIDES ----------------------------- 
+        //
+
+        public override bool Equals(object obj)
+        {
+            if (obj is ByteDBPacket packet)
+                return packet.Payload == Payload;
+            
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return Payload.GetHashCode();
+        }
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            builder.Append("Header: ");
+            builder.Append(string.Join(", ", Header));
+
+            builder.AppendLine();
+
+            builder.Append("Payload: ");
+            builder.Append(string.Join(", ", Payload));
+
+
+            return builder.ToString();
         }
     }
 }
--- ByteDBServer/Core/Server/Connection/Models/ByteDBProtocol.cs
+++ ByteDBServer/Core/Server/Connection/Models/ByteDBProtocol.cs
@@ -1,7 +1,14 @@
-﻿namespace ByteDBServer.Core.Server.Connection.Models
-{
-    internal class ByteDBProtocol
-    {
+﻿using ByteDBServer.Core.DataTypes;
 
+namespace ByteDBServer.Core.Server.Connection.Models
+{
+    internal abstract class ByteDBProtocol
+    {
+        public Int1 Version { get; set; }
+
+        public ByteDBProtocol(int version)
+        {
+            Version = new Int1(version);
+        }
     }
 }
