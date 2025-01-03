aa59a63984afa5c35179fd09f637b93a9f7c08b3
Mysteriusz <wixxxiw730@gmail.com>
2025-01-03 15:39:14 +0100

    early handshaking packet and protocol

--- ByteDBServer/Core/Server/Connection/Handshake/ByteDBWelcomePacketV1.cs
+++ ByteDBServer/Core/Server/Connection/Handshake/ByteDBWelcomePacketV1.cs
@@ -1,13 +1,21 @@
-﻿using ByteDBServer.Core.Server.Connection.Models;
+﻿using ByteDBServer.Core.DataTypes;
+using ByteDBServer.Core.Server.Connection.Models;
 using System.IO;
 
 namespace ByteDBServer.Core.Server.Connection.Handshake
 {
     internal class ByteDBWelcomePacketV1 : ByteDBPacket
     {
-        public ByteDBWelcomePacketV1(Stream stream, int bufferSize)
+        public ByteDBWelcomePacketV1() 
         {
-            this.Write(stream);
+            //
+            // ----------------------------- WELCOME PACKET STRUCTURE ----------------------------- 
+            //
+
+            // Server Version
+            AddRange(new NullTerminatedString(ByteDBServer.Version).Bytes);
+
+            // 
         }
 
         public override void Dispose()
