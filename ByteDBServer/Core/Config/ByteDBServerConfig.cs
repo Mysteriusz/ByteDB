083c2b665013d4fe7a03ffa084b696a89756b6c4
Mysteriusz <wixxxiw730@gmail.com>
2025-01-03 15:37:29 +0100

    new XML, port, version, encoding, max connections and server capabilities intiger

--- ByteDBServer/Core/Config/ByteDBServerConfig.cs
+++ ByteDBServer/Core/Config/ByteDBServerConfig.cs
@@ -1,4 +1,9 @@
-﻿using System.Xml;
+﻿using ByteDBServer.Core.DataTypes;
+using DataTypesTesting.DataTypes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
 
 namespace ByteDBServer.Core.Config
 {
@@ -11,9 +16,35 @@ namespace ByteDBServer.Core.Config
             XmlDocument config = new XmlDocument();
             config.LoadXml(ConfigPath);
 
+            // Strings
             ServerVersion = config.Attributes.GetNamedItem("ServerVersion").Value;
+
+            // Intigers
+            MaxConnections = int.Parse(config.Attributes.GetNamedItem("Port").Value);
+            Port = int.Parse(config.Attributes.GetNamedItem("MaxConnections").Value);
+            ServerCapabilitiesInt = uint.Parse(config.Attributes.GetNamedItem("ServerCapabilities").Value);
+
+            // Other
+            Encoding = ByteDBEncoding.EncodingType[config.Attributes.GetNamedItem("Encoding").Value];
         }
 
         public static string ServerVersion { get; private set; }
+        
+        public static int MaxConnections { get; private set; }
+        public static int Port { get; private set; }
+        public static Int4 ServerCapabilitiesInt { get; private set; }
+
+        public static Encoding Encoding { get; private set; }
+
+        public static List<TEnum> ReadFlags<TEnum>(uint flagValue) where TEnum : Enum
+        {
+            List<TEnum> result = new List<TEnum>();
+
+            foreach (TEnum enumValue in Enum.GetValues(typeof(TEnum)))
+                if ((flagValue & Convert.ToUInt32(enumValue)) != 0)
+                    result.Add(enumValue);
+
+            return result;
+        }
     }
 }
