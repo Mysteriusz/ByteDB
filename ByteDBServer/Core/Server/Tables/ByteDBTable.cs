18539c184489a2f959f7b0bca21877ef14ba3d09
Mysteriusz <wixxxiw730@gmail.com>
2025-01-13 11:51:32 +0100

    ByteDBTable and ByteDBTableEntry and xml table example

---
+++ ByteDBServer/Core/Server/Tables/ByteDBTable.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Linq;
+using System;
+using ByteDBServer.Core.Server.Tables;
+
+namespace ByteDBServer.Core.Server.Databases
+{
+    internal class ByteDBTable
+    {
+        public string TableName { get; private set; }
+        public string TableFullPath { get; private set; }
+
+        public List<string> ColumnNames { get; private set; }
+        public List<Type> ColumnTypes { get; private set; }
+
+        public ByteDBTable() { }
+
+        public void AddRow(List<string> columns, List<string> values)
+        {
+            XDocument tableDoc = XDocument.Load(TableFullPath);
+            tableDoc.Root.Add(new ByteDBTableEntry(columns, values).GetElement());
+            tableDoc.Save(TableFullPath);
+        }
+
+        public static ByteDBTable Load(string tableFullPath)
+        {
+            ByteDBTable table = new ByteDBTable();
+            XDocument tableDoc = XDocument.Load(tableFullPath);
+
+            table.TableFullPath = tableFullPath;
+            table.TableName = tableDoc.Root.Attribute("Name").Value;
+            table.ColumnNames = tableDoc.Root.Attribute("ColumnNames").Value.Split(',').ToList();
+            table.ColumnTypes = tableDoc.Root.Attribute("ColumnTypes").Value.Split(',').Select(t => ByteDBServerInstance.TableValueTypes[t]).ToList();
+            
+            return table;
+        }
+    }
+}
---
+++ ByteDBServer/Core/Server/Tables/ByteDBTableEntry.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace ByteDBServer.Core.Server.Tables
+{
+    internal class ByteDBTableEntry
+    {
+        public const string EntryName = "Entry";
+        public List<XAttribute> Attributes { get; } = new List<XAttribute>();
+
+        public ByteDBTableEntry(List<string> columns, List<string> values)
+        {
+            for (int i = 0; i < columns.Count; i++)
+                Attributes.Add(new XAttribute(columns[i], values[i]));
+        }
+
+        public XElement GetElement()
+        {
+            XElement element = new XElement(EntryName);
+
+            foreach (var attr in Attributes)
+                element.SetAttributeValue(attr.Name, attr.Value);
+
+            return element;
+        }
+    }
+}
